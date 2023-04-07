3407558c741c73edc5afb1a6d8d26003abe2991b
crimson-tea <91731135+crimson-tea@users.noreply.github.com>
2023-04-07 21:44:20 +0900

    update: 新apiへの対応

---
+++ AniMa/JsonObjects/EpisodeGroups.cs
@@ -0,0 +1,25 @@
+﻿namespace AniMa.JsonObjects;
+
+
+public class EpisodeGroups
+{
+    public Episodegroupcontent[] episodeGroupContents { get; set; }
+}
+
+public class Episodegroupcontent
+{
+    public string id { get; set; }
+    public Thumbcomponent thumbComponent { get; set; }
+    public Episode episode { get; set; }
+    public int type { get; set; }
+    public Video video { get; set; }
+    public Info info { get; set; }
+}
+
+public class Video
+{
+    public Term[] terms { get; set; }
+    public int broadcastRegionPolicy { get; set; }
+    public int releaseYear { get; set; }
+}
+
